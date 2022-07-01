fcc43fe75e6e8381a18125e8a904d9ab4e93e7dc
ajax <ajaxliu@chancetop.com>
2022-07-01 09:34:41 +0800

    code optimize

--- src/main/java/com/ajax/websocketdemo/controller/ChatController.java
+++ src/main/java/com/ajax/websocketdemo/controller/ChatController.java
@@ -7,19 +7,21 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
+import static com.ajax.websocketdemo.Rooms.PUBLIC_ROOM;
+
 /**
  * @author ajax
  */
 @Controller
 public class ChatController {
     @MessageMapping("/chat.sendMessage")
-    @SendTo("/topic/public")
+    @SendTo(PUBLIC_ROOM)
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
         return chatMessage;
     }
 
     @MessageMapping("/chat.addUser")
-    @SendTo("/topic/public")
+    @SendTo(PUBLIC_ROOM)
     public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
         // Add username in web socket session
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
